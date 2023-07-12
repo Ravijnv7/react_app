f093a86fbe9711a4d256a13cb87b64b461e6f9ad
Ravi kumar <ravikumar@Ravis-MacBook-Air.local>
2023-07-12 09:58:39 +0530

    learn the basic of rendering and making element using react

--- App.js
+++ App.js
@@ -1,37 +1,15 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
-  
-  //JSX
-  
-  console.log(parent); // object  
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+//React Element=>object-when we render into dom then it becomes an HTML ELEMENT
+
+const heading=React.createElement(
+  "h1"
+,{id:"Heading"},
+"Namste react");
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
   
