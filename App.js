05ed199ff961e0f00dd0eb0cede6a767a04d1c11
Ravi kumar <ravikumar@Ravis-MacBook-Air.local>
2023-07-13 23:30:53 +0530

    food-app_1st_day

--- App.js
+++ App.js
@@ -2,55 +2,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element=>object-when we render into dom then it becomes an HTML ELEMENT
-
-
-//JSX=>we can use jsx to create html element using this easily in react
-// mearge the html and javascript
-//create h1 tag using jsx// jsx is not html inside javascript,html like syntax or xml
-
-//jsx(transpiled before it reaches the js engine)=>transpiling is done by parcel-Babel 
-//react element=> 
-const number=1000;
-const Headingcomponent =()=>(
-  <div id="container">
-   <h2>{100+200}</h2> 
-   <h1 className="heading">namste ravi</h1>
-  </div>
-);
-const elem=<span>react element</span>
-const title= (
-<h1 className="heading" tabIndex="5">
-  {elem}
-  <Headingcomponent/>
-  Namstre ravi how are you
-</h1>
-);
-
-//React component
-//class based component
-//fucntional based 
-//react functional component=>simple javascript function
- /*const Headingcomponent=()=>{
-  return <h1 className="heading">Namste ravi functional component</h1>
- };//return jsx component;
+/**
+ * header(-logo component nav item)
+ * body
+ * -search (bar ->input)
+ * restaurant card (container)
+ * {img,
+ * name of res,star rating,cuisine,delivery time
+ * }
+ * footer->copyright disclamier,address,contact links
+ *
  */
-//react fragmaent;
-//functional component;
-const Sa =()=>(
-  <>
-    <div id="container1">
-      <h1>ravi kumar</h1>
+const Header =()=>{
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/d0/8e/3d/d08e3d52-6a71-d545-c853-4e2c5568d307/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x600wa.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>cart</li>
+        </ul>
+
+
+      </div>
     </div>
-    <div id="container2">
-      <h2>saching</h2>
+  )
+}
+const ReastaurantCard =()=>{
+  return(
+<div className="res-cards">
+  <h3>Meghna foods</h3>
+</div>
+  )
+}
+const Body =()=>{
+  return(
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        <ReastaurantCard/>
+      </div>
+       
     </div>
-    </>
-);
- 
+
+  )
+}
+const AppLayout =()=>{
+  return(
+    <div className="app">
+      <Header/>
+      <Body/>
+    </div>
+  )
+}
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Sa/>);
+root.render(<AppLayout/>);
   
